51a3ea78f47dd2fab682594781ee28b6c48caa9d
John M. Harris <johnmh@openblox.org>
2016-10-09 15:53:45 -0400

    Fix style in openblox.cpp
    Convert spaces to tabs in indentation of the main function,
    specifically in getopt handling.

--- src/openblox.cpp
+++ src/openblox.cpp
@@ -31,48 +31,48 @@ using namespace OB;
 
 int main(int argc, char* argv[]){
 	static struct option long_opts[] = {
-        {"version", no_argument, 0, 'v'},
-        {"help", no_argument, 0, 'h'},
+		{"version", no_argument, 0, 'v'},
+		{"help", no_argument, 0, 'h'},
 		{"script", required_argument, 0, 0},
-        {0, 0, 0, 0}
-    };
+		{0, 0, 0, 0}
+	};
 
 	int opt_idx = 0;
 
 	int c;
-    while(1){
-        c = getopt_long(argc, argv, "vh", long_opts, &opt_idx);
+	while(1){
+		c = getopt_long(argc, argv, "vh", long_opts, &opt_idx);
 
-        if(c == -1){
-            break;
-        }
+		if(c == -1){
+			break;
+		}
 
-        switch(c){
-            case 'v': {
-                puts(PACKAGE_STRING);
-                puts("Copyright (C) 2016 John M. Harris, Jr.");
-                puts("This is free software. It is licensed for use, modification and");
-                puts("redistribution under the terms of the GNU General Public License,");
-                puts("version 3 or later <https://gnu.org/licenses/gpl.html>");
-                puts("");
-                puts("Please send bug reports to: <" PACKAGE_BUGREPORT ">");
-                exit(EXIT_SUCCESS);
-                break;
-            }
-            case 'h': {
-                puts(PACKAGE_NAME);//TODO: " - Description"?
-                printf("Usage: %s [options]\n", argv[0]);
+		switch(c){
+			case 'v': {
+				puts(PACKAGE_STRING);
+				puts("Copyright (C) 2016 John M. Harris, Jr.");
+				puts("This is free software. It is licensed for use, modification and");
+				puts("redistribution under the terms of the GNU General Public License,");
+				puts("version 3 or later <https://gnu.org/licenses/gpl.html>");
+				puts("");
+				puts("Please send bug reports to: <" PACKAGE_BUGREPORT ">");
+				exit(EXIT_SUCCESS);
+				break;
+			}
+			case 'h': {
+				puts(PACKAGE_NAME);//TODO: " - Description"?
+				printf("Usage: %s [options]\n", argv[0]);
 				puts("   --script       Add init script");
-                puts("   -v, --version  Prints version information and exits");
-                puts("   -h, --help     Prints this help text and exits");
-                puts("");
-                puts("Options are specified by doubled hyphens and their name or by a single");
-                puts("hyphen and the flag character.");
-                puts("");
-                puts("Please send bug reports to: <" PACKAGE_BUGREPORT ">");
-                exit(EXIT_SUCCESS);
-                break;
-            }
+				puts("   -v, --version  Prints version information and exits");
+				puts("   -h, --help     Prints this help text and exits");
+				puts("");
+				puts("Options are specified by doubled hyphens and their name or by a single");
+				puts("hyphen and the flag character.");
+				puts("");
+				puts("Please send bug reports to: <" PACKAGE_BUGREPORT ">");
+				exit(EXIT_SUCCESS);
+				break;
+			}
 			case 0: {
 				if(long_opts[opt_idx].flag != 0){
 					break;
@@ -83,15 +83,15 @@ int main(int argc, char* argv[]){
 				}
 				break;
 			}
-            case '?': {
-                exit(EXIT_FAILURE);
-                break;
-            }
-            default: {
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+			case '?': {
+				exit(EXIT_FAILURE);
+				break;
+			}
+			default: {
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 	
 	OBEngine* engine = new OBEngine();
 	engine->init();
