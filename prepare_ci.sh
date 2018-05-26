b802d83432f8c684270bddf5d0dbc95015da7da6
John M. Harris, Jr <johnmh@splentity.com>
2018-05-26 18:17:41 -0400

    Move some stuff to a bash script

---
+++ prepare_ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl --header "PRIVATE-TOKEN: $DOWNLOAD_TOKEN" "https://git.splentity.com/api/v4/projects/openblox%2Flibopenblox/jobs/artifacts/master/download?job=build" -o libopenblox.zip
+
+unzip libopenblox.zip
+
+sed "1cprefix=$(pwd)" artifacts/libopenblox.pc > libopenblox.pc
